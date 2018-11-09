210576c56bfae12ae9a30f9fcbf4b38e6f2ee5be
Bogdan <dastan_krimea@mail.ru>
2018-11-09 20:34:41 +0300

    интерактивная карта с маркером iframe

--- source/js/map.js
+++
@@ -1,9 +0,0 @@
-google.maps.event.addDomListener(window, 'load', init);
-function init() {
-  var mapOptions = {
-  zoom: 16,
-  center: new google.maps.LatLng(59.93877819999999, 30.32319670000004)
-  };
-  var mapElement = document.getElementById('map');
-  var map = new google.maps.Map(mapElement, mapOptions);
-}
