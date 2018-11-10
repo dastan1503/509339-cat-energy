7d45540f5a4e01272a5fb2592f80418d2a52b4fc
Bogdan <dastan_krimea@mail.ru>
2018-11-10 13:42:30 +0300

    style-min.css

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,22 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var rename = require("gulp-rename")
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var csso = require("gulp-csso")
+var imagemin = require("gulp-imagemin")
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationlevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -14,6 +27,9 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(gulp.dest("source/css"))
+    .pipe(csso())
+    .pipe(rename("style-min.css"))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
